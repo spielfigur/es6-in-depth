d3b356cdbc3170cb6d3c1d0b77e5e6a81891bee2
Spielfigur <hajo.voet@t-online.de>
2019-01-18 15:59:52 +0100

    till episode 40
    initial development project with:
    
    * block scoping
    * const and let

---
+++ README.md
@@ -0,0 +1,9 @@
+* ES6 in depth
+
+following the udemy tutorial at https://www.udemy.com/es6-in-depth
+
+*** Start the build process
+```
+npm run prod
+npm run dev
+```
---
+++ app/index.js
@@ -0,0 +1,24 @@
+let ticketLimit = 200;
+ticketLimit += 200;
+console.log(ticketLimit);
+
+const emails= [
+    'frodo@localhost.com',
+    'samwise@localhost.com',
+    'merry@localhost.com'
+];
+emails.push('pippin@localhost.com');
+console.log(emails);
+
+function hello() {
+    let message = 'Hello!';
+    console.log(message);
+}
+
+function greeting() {
+    let message = 'How are you?';
+    console.log(message);
+}
+
+hello();
+greeting();
